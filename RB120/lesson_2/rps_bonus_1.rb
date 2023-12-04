fda5285299dabfb389dc149ed9f24a8312a4cd0f
Ilke Alexander <softsilkworm@gmail.com>
2023-12-04 20:53:44 +0200

    changes

--- RB120/lesson_2/rps_bonus_1.rb
+++ RB120/lesson_2/rps_bonus_1.rb
@@ -1,37 +1,58 @@
-module UserInterface
-  def prompt
-  end
-  
-  def get_name
-    n = ''
-    loop do
-      puts "What's your name"
-      n = gets.chomp
-      break unless n.empty? || n.strip.empty?
-      puts "Sorry, must enter a value."
-    end
-    n.capitalize
-  end
-
-  def get_opponent
-  end
-
-  def get_move
-  end
-
-  def get_play_to_score
-  end
-end
-
 module Displayable
+  WELCOME_MESSAGE = <<-TEXT
+  -------------------------------------------
+  Welcome to ROCK PAPER SCISSORS LIZARD SPOCK
+  -------------------------------------------
+  TEXT
+
+  BANNER = <<-TEXT
+  --------------------------------
+  ROCK PAPER SCISSORS LIZARD SPOCK
+  --------------------------------
+  TEXT
+
+  RULES = <<-TEXT
+  Before we begin here are a few things about the game you need to know:
+  - you'll be playing against the computer
+  - you get to choose how many rounds each game consists of
+
+  Rules:
+  - Scissors cuts Paper
+  - Paper covers Rock
+  - Rock crushes Lizard
+  - Lizard poisons Spock
+  - Spock smashes Scissors
+  - Scissors decapitates Lizard 
+  - Lizard eats Paper
+  - Paper disproves Spock
+  - Spock vaporizes Rock
+  - Rock crushes Scissors
+  TEXT
+
+  GOODBYE_MESSAGE = <<-TEXT
+  -----------------------------------------------------------------
+  Thank you for playing ROCK PAPER SCISSORS LIZARD SPOCK! Good bye.
+  -----------------------------------------------------------------
+  TEXT
+
+  def prompt(message)
+    "=> #{message}"
+  end
+
   def display_welcome_message
-    puts "Welcome to Rock, Paper, Scissors!"
+    puts WELCOME_MESSAGE
+  end
+
+  def display_banner
+    puts BANNER
   end
 
   def display_rules
+    puts RULES
   end
 
   def display_round_number
+    puts "ROUND NUMBER: #{round_counter}".center(32)
   end
 
   def display_moves
@@ -58,6 +79,7 @@ module Displayable
   end
 
   def display_goodbye_message
+    puts GOODBYE_MESSAGE
   end
 end
 
@@ -103,11 +125,11 @@ class Score
   end
 
   def update
-    if human.move > computer.move
-      human.score += 1
-    elsif human.move < computer.move
-      computer.score += 1
-    end
+    self.points += 1
+  end
+
+  def reset
+    @points = 0
   end
 end
 
@@ -115,13 +137,23 @@ class Player
   attr_accessor :move, :name, :score
 
   def initialize
-    @name = get_name
+    set_name
     @score = Score.new
   end
 end
 
 class Human < Player
-  include UserInterface
+  def set_name
+    puts Displayable::WELCOME_MESSAGE
+    n = ''
+    loop do
+      puts "What's your name"
+      n = gets.chomp
+      break unless n.empty? || n.strip.empty?
+      puts "Sorry, must enter a value."
+    end
+    self.name = n
+  end
 
   def choose
     choice = nil
@@ -157,31 +189,32 @@ class RPSGame
     @history = History.new
   end
 
-  # def play_again?
-  #   answer = nil
-  #   loop do
-  #     puts "Would you like to play again? (y/n)"
-  #     answer = gets.chomp
-  #     break if ['y', 'n'].include?(answer.downcase)
-  #     puts "Sorry, must be y or n."
-  #   end
+  def play_again?
+    answer = nil
+    loop do
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp
+      break if ['y', 'n'].include?(answer.downcase)
+      puts "Sorry, must be y or n."
+    end
 
-  #   return false if answer.downcase == 'n'
-  #   return true if answer.downcase == 'y'
-  # end
+    return false if answer.downcase == 'n'
+    return true if answer.downcase == 'y'
+  end
 
   def play
-    display_welcome_message
-
-    loop do
-      human.choose
-      computer.choose
-      display_moves
-      display_winner
-      update_score
-      display_score
-      # break unless play_again?
-      break if human.score == 5 || computer.score == 5
+    display_banner
+    loop do 
+      loop do
+        human.choose
+        computer.choose
+        display_moves
+        display_winner
+        update_score
+        display_score
+        break if human.score == 5 || computer.score == 5
+      end
+      break unless play_again?
     end
     display_goodbye_message
   end
